39ee36faf462d2c07a52a8596296e651cdd35713
chenshoumao <1223991507@qq.com>
2017-07-17 17:17:08 +0800

    45

--- src/com/solar/dao/impl/LandDaoImpl.java
+++ src/com/solar/dao/impl/LandDaoImpl.java
@@ -30,7 +30,7 @@ import com.solar.utils.Zip;
 
 public class LandDaoImpl implements LandDao {
 
-	// 版本命名规范，如10.0.0_db_release_20170713，可是可分为四个部分
+	// 版本命名规范，如10.0.0_db_release_20170713，可是可分为四个部分123
 	private static int length = 4;
 
 	private final String VERSION = "version.txt";
